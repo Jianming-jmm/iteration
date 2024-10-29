646ec646d2e31f07bb4eaf658adbe8ea071a7da4
Jianming-jmm <jw4693@cumc.columbia.com>
2024-10-29 23:21:29 +0800

    10.29

--- nsduh_table_format.R
+++ nsduh_table_format.R
@@ -1,9 +1,9 @@
-nsduh_table_format <- function(html, table_num, table_name){
+nsduh_table_format <- function(html, table_num){
   out_table = nsduh_html |> 
     html_table() |> 
     nth(table_num) |>
     slice(-1)|> 
-    mutate(drug = table_name)
+    select(-contains('P Value'))
   
   return(out_table)
 }
